276c6d2eea7b5ce0c60c3da16dcf5c4c58bb4ce0
zhaojie <1979170837@qq.com>
2024-01-13 09:37:28 +0800

    单向链表练习-移除链表中的元素

--- data-structure/src/main/java/org/sperri/algorithms/data_structure/list/example/移除单向链表元素.java
+++ data-structure/src/main/java/org/sperri/algorithms/data_structure/list/example/移除单向链表元素.java
@@ -43,30 +43,61 @@ public class 移除单向链表元素 {
      * @param val
      * @return
      */
-    public ListNode removeElements2(ListNode head, int val) {
+    public static ListNode removeElements2(ListNode head, int val) {
         if (head == null) {
             return null;
         }
 
-        ListNode prev = null;
+        ListNode sentinel = new ListNode(-1, head);
+        ListNode prev = sentinel;
         ListNode curr = head;
         while (curr != null) {
-            if (curr == head && curr.val == val) {
-                head = head.next;
-                curr = head;
-                prev = null;
-            } else if (curr.val == val) {
+            if (curr.val == val) {
                 prev.next = curr.next;
                 curr = prev.next;
             } else {
                 prev = curr;
-                curr = curr.next;
+                curr = prev.next;
             }
         }
         return head;
     }
 
-    public static void main(String[] args) {
+    /**
+     * 方法二：递归删除
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public static ListNode removeElements3(ListNode head, int val) {
 
+        if (head == null) {
+            return null;
+        }
+        if (head.val == val) {
+            return head.next;
+        } 
+        return removeElements3(head.next, val);
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.printf("%-4d", head.val);
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(6, null);
+        ListNode b = new ListNode(2, a);
+        ListNode c = new ListNode(3, b);
+        ListNode d = new ListNode(6, c);
+        ListNode e = new ListNode(2, d);
+        ListNode f = new ListNode(1, e);
+
+        printList(f);
+        printList(removeElements3(f, 6));
     }
 }
