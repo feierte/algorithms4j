759ce08da103cee44a0a34d3d32aa6afaab6ac98
zhaojie <1979170837@qq,com>
2026-05-26 20:38:59 +0800

    LeetCode 53

---
+++ data-structure/src/main/java/org/sperri/algorithms/example/leetcode/Leetcode53_最大子数组和.java
@@ -0,0 +1,41 @@
+package org.sperri.algorithms.example.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author Jie Zhao
+ * @date 2026/5/26 20:00
+ */
+public class Leetcode53_最大子数组和 {
+
+
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            maxSum = Math.max(maxSum, currentSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+
+}
