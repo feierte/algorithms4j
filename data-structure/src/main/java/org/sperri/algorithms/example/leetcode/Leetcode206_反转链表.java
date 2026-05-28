b060b615a2ba9d011be87e337ad9a422dba5093f
zhaojie <1979170837@qq,com>
2026-05-28 15:41:14 +0800

    LeetCode 206

---
+++ data-structure/src/main/java/org/sperri/algorithms/example/leetcode/Leetcode206_反转链表.java
@@ -0,0 +1,81 @@
+package org.sperri.algorithms.example.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Jie Zhao
+ * @date 2026/5/28 15:01
+ */
+public class Leetcode206_反转链表 {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    /**
+     * 思路：将链表中的元素放到栈中，通过栈的特性反转链表，然后在组合成一个新的链表。
+     * 没有做出来
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        Deque<ListNode> queue = new LinkedList<>();
+        ListNode cur = head;
+        while (cur != null) {
+            queue.addLast(cur);
+
+            ListNode next = cur.next;
+            if (next != null) {
+                cur.next = null;
+                cur = next;
+            }
+//            if (cur != null) {
+//                cur.next = queue.peekLast();
+//            }
+        }
+
+        ListNode reverseList = queue.getLast();
+        ListNode p = reverseList;
+        while (!queue.isEmpty()) {
+            p.next = queue.getLast();
+        }
+
+        return reverseList;
+    }
+
+
+    /**
+     * 三指针法
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        ListNode p = null;
+        ListNode next;
+        while (head != null) {
+            next = head.next;
+            head.next = p;
+
+            p = head;
+            head = next;
+        }
+        return p;
+    }
+}
